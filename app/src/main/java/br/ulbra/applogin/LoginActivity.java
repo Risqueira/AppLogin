44d1664326e4ac8dcca5c1de1c1057f772fa50e7
Risqueira <henrique.mrodrigues77@gmail.com>
2026-05-25 22:25:49 -0300

    texto

--- app/src/main/java/br/ulbra/applogin/LoginActivity.java
+++ app/src/main/java/br/ulbra/applogin/LoginActivity.java
@@ -1,4 +1,54 @@
 package br.ulbra.applogin;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edLogin, edPass;
+    Button btTelaLogin;
+    DBHelper db;
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate (savedInstanceState);
+        setContentView(R.layout.activity_login);
+        db = new DBHelper(this);
+        edLogin = (EditText) findViewById(R.id.edtInserirLogin);
+        edPass = (EditText) findViewById(R.id.edtInserirSenha);
+        btTelaLogin = (Button) findViewById(R.id.btnTelaLogin);
+
+        btTelaLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = edLogin.getText().toString();
+                String password = edPass.getText().toString();
+
+                if (userName.equals("")){
+                    Toast.makeText(LoginActivity.this, "Usuário não inserido, tente novamente",
+                            Toast.LENGTH_SHORT).show();
+
+                } else if (password.equals("")) {
+
+                    Toast.makeText(LoginActivity.this, "Senha não inserida, tente novamente",
+                            Toast.LENGTH_SHORT).show();
+
+                } else {
+                    String res = db.validarLogin(userName, password);
+
+                    if (res.equals("OK")) {
+                        Toast.makeText(LoginActivity.this, "Login OK!!",
+                                Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Login ou Senha errados!!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/br/ulbra/applogin/RegistrarActivity.java
+++ app/src/main/java/br/ulbra/applogin/RegistrarActivity.java
@@ -1,4 +1,58 @@
 package br.ulbra.applogin;
 
-public class RegistrarActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrarActivity extends AppCompatActivity {
+
+    EditText edNome, edUser, edPas1, edPas2;
+    Button btSalvar;
+    DBHelper db;
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+        db = new DBHelper(this);
+
+        edNome = (EditText) findViewById (R.id.edtNome);
+        edUser = (EditText) findViewById (R.id.edtLogin);
+        edPas1 = (EditText) findViewById (R.id.edtSenha);
+        edPas2 = (EditText) findViewById (R.id.edtSenha2);
+
+        btSalvar = (Button) findViewById (R.id.btnSalvar);
+
+        btSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName =  edUser.getText().toString();
+                String pas1 = edPas1.getText().toString();
+                String pas2 = edPas2.getText().toString();
+
+                if (userName.equals("")){
+                    Toast.makeText(RegistrarActivity.this, "Insira o LOGIN DO USUÁRIO", Toast.LENGTH_SHORT).show();
+                } else if (pas1.equals("") || pas2.equals("")) {
+                    Toast.makeText(RegistrarActivity.this, "Insira a SENHA DO USUÁRIO",
+                            Toast.LENGTH_SHORT).show();
+
+                } else if (!pas1.equals(pas2)) {
+                    Toast.makeText(RegistrarActivity.this, "As senhas não correspondem ao login do usuário",
+                            Toast.LENGTH_SHORT).show();
+
+                } else {
+                    long res = db.criarUtilizador(userName, pas1);
+                    if (res > 0) {
+                        // nesta parte você poderá chamar a tela principal do sistema autenticado
+                        Toast.makeText(RegistrarActivity.this, "Registro OK", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(RegistrarActivity.this, "Senha invaliada!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
 }
